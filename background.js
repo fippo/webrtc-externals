d4dbb9830f0a24306e8843df14a6f2c36b5a2bb8
Philipp Hancke <fippo@appear.in>
2017-07-24 10:12:33 +0200

    use browser instead of chrome
    makes things open in Firefox and Edge.

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
-chrome.runtime.onConnect.addListener(function (channel) {
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/runtime/onConnect
+if (typeof browser === 'undefined') {
+    browser = chrome;
+}
+browser.runtime.onConnect.addListener(function (channel) {
     channel.onMessage.addListener(function (message, port) {
         if (message[0] !== 'WebRTCExternals') return;
         console.log(new Date(), message[1], message[2], message[3], JSON.parse(message[4]));
     });
 });
 
-/*
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('background.html'), 'selected': true});
+browser.browserAction.onClicked.addListener(function(tab) {
+    browser.tabs.create({'url': browser.extension.getURL('background.html')});
 });
-*/
