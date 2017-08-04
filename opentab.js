418ccb7f412baa5dba25427fb811e04dd920892e
Philipp Hancke <fippo@appear.in>
2017-08-04 22:26:56 +0200

    make at least one port available

--- opentab.js
+++ opentab.js
@@ -6,3 +6,7 @@ browser.browserAction.onClicked.addListener(function() {
     browser.tabs.create({'url': browser.extension.getURL('background.html')});
 });
 
+browser.runtime.onConnect.addListener(function (channel) {
+    // adds a dummy listener so that there is at least one listener.
+    // TODO: could be used to persist data for background.html
+});
